93702a999636097bce92213ab7ec5e20c91a2673
Sarah Temidayo <64485891+Dhanidayo@users.noreply.github.com>
2021-04-28 17:43:41 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,44 +1,42 @@
-async function getData() {
-    try{
-
-        const response = await fetch("https://api.edamam.com/search?q=soup&app_id=790ebbcb&app_key=b1cae523fb6b305930d845edc623c9de&from=0&to=100");
-        const data = await response.json()
-        return data
-    } catch(err){
-        console.log("error encountered in getData Function", err)
-    }
-
+async function getData(searchTerm) {
+  try {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=${searchTerm}&app_id=790ebbcb&app_key=b1cae523fb6b305930d845edc623c9de&from=0&to=100`
+    );
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (err) {
+    console.log("error encountered in getData Function", err);
   }
+}
 
-  // getData()
-
-  //function to define innerHTML for the web content.
-  async function showData() {
-      try{
-
-          // let recipeData = await getData();
-          // let recipes = recipeData;
-          let recipes = await getData();
-          let html = "";
-        //   console.log(recipes.hits)
-          // recipes.forEach((recipeData) =>
-          //   function e() {
-          recipes.hits.forEach((e) => {
-              let webContent = `<div class="recipe">
+//function to define innerHTML for the web content.
+async function showData() {
+  try {
+    const searchTerm = document.getElementById("searchBox").value || "" ;
+    console.log("search item", searchTerm);
+    if (searchTerm !== "") { 
+        console.log("In IF statement")
+      let recipes = await getData(searchTerm);
+      let html = ""; 
+      recipes.hits.forEach((e) => { 
+        let webContent = `<div class="recipe">
                                     <a href=${e.recipe.url}' class="recipe-link" target="_blank">
                                     <img class="recipe-image" src="${e.recipe.image}"\>
                                     <p class="recipe-name">${e.recipe.label}</p>
                                     </a>
-                                </div>`
-      
-              html += webContent;
-            })
-            // console.log(html);
-            
-            const searchResult = document.querySelector("#myContainer");
-            searchResult.innerHTML = html;
-        }catch(err){
-            console.log("error encountered in show Data Function",err)
-        }
-    }
-    document.getElementById("searchBtn").addEventListener('click', showData)
+                            </div>`;
+
+        html += webContent;
+      });
+      const searchResult = document.querySelector("#myContainer");
+      searchResult.innerHTML = html;
+    } else {
+        console.log("No result")
+     }
+  } catch (err) {
+    console.log("error encountered in show Data Function", err);
+  }
+}
+document.getElementById("searchBtn").addEventListener("click", showData);
